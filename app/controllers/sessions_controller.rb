df43f1c50d7cc8e79f39a28dfb207e47d37ad160
Proto <protosacq@gmail.com>
2015-12-27 16:09:23 -0800

    Add sessions and remove dead files.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
+    # console
   end
 
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
       redirect_to '/', notice: "Login successful."
     else
       flash.now.alert = "Email or password incorrect."
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 end
